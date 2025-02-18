3421e8cfaca96588896a8ad697a191b2d32ae5d1
Husan <samadovxusan9013@gmail.com>
2025-02-18 14:41:51 +0500

    Add HostConfiguration

---
+++ Barber.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,87 @@
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barber.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static readonly ICollection<Assembly> Assemblies;
+
+    static HostConfiguration()
+    {
+        Assemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
+        Assemblies.Add(Assembly.GetExecutingAssembly());
+    }
+    
+    
+    /// <summary>
+    /// Adds client-related infrastructure services to the web application builder.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns>Application builder</returns>
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+    
+    /// <summary>
+    ///  Configures exposers including controllers and routing.
+    /// </summary>
+    /// <param name="builder">>Application builder</param>
+    /// <returns>Application builder</returns>
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<ApiBehaviorOptions>(
+            options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            }
+        );
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+        
+        return builder;
+    }
+    
+
+    /// <summary>
+    /// Enables CORS middleware in the web application pipeline.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    private static WebApplication UseCors(this WebApplication app)
+    {
+        app.UseCors("AllowSpecificOrigin");
+
+        return app;
+    }
+
+    /// <summary>
+    /// Add Controller middleWhere
+    /// </summary>
+    /// <param name="app">Application host</param>
+    /// <returns>Application host</returns>
+    private static WebApplication UseDevTools(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+    
+    /// <summary>
+    /// Add Controller middleWhere
+    /// </summary>
+    /// <param name="app">Application host</param>
+    /// <returns>Application host</returns>
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
