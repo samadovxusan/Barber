62e07f66dcb5349621abbfa51f36b33144c7fa96
Husan <samadovxusan9013@gmail.com>
2025-03-10 10:33:10 +0500

    Add imgasModel

---
+++ Barber.Persistence/Migrations/20250310053101_Add_ImagesModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Barber.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_ImagesModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Imageses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Imageses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Imageses",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Imageses");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Imageses");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Imageses");
+        }
+    }
+}
