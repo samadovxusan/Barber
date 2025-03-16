9bcb0852a85c0a73a1c0e7deb6c8cf496dba9e6b
Husan <samadovxusan9013@gmail.com>
2025-03-16 23:05:21 +0500

    Add Auth/Barber and Auth.Admin

---
+++ Barber.Application/Users/Models/BarbersCreate.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+using Barber.Domain.Enums;
+
+namespace Barber.Application.Users.Models;
+
+public class BarberCreate
+{
+    public string FullName { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string PhoneNumber { get; set; } = default!;
+    [JsonIgnore]
+    public Role Roles { get; set; } = Role.Barber;
+}
