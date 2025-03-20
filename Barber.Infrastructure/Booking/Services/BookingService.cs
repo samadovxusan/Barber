e34be5182327880c04b4a894494e37281c531cb2
Husan <samadovxusan9013@gmail.com>
2025-03-20 16:16:26 +0500

    Changed BarberApprouch

--- Barber.Infrastructure/Booking/Services/BookingService.cs
+++ Barber.Infrastructure/Booking/Services/BookingService.cs
@@ -63,6 +63,9 @@ public class BookingService(IBookingRepositoriess repositoriess,  AppDbContext _
             if (existingBooking != null)
                 throw new Exception("Ushbu vaqt allaqachon band qilingan.");
             
+            
+            await _context.Bookings.AddAsync(booking, cancellationToken: cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken: cancellationToken);
             return true;
 
         }
