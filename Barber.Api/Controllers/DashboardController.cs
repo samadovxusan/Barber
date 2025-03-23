2da0718840433e5dc6bc437c1b2829cbf5284a97
Husan <samadovxusan9013@gmail.com>
2025-03-23 23:13:42 +0500

    Fixed DashboardController

--- Barber.Api/Controllers/DashboardController.cs
+++ Barber.Api/Controllers/DashboardController.cs
@@ -1,12 +1,15 @@
 ﻿using Barber.Application.Dashboard;
+using Barber.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Barber.Api.Controllers;
-
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class DashboardController(IDashboardService service) : ControllerBase
 {
+    [Authorize(Roles = "Admin")]
     [HttpGet]
     public async ValueTask<IActionResult> Get() =>
         Ok(await service.GetAllCount());
