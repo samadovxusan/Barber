2c44fe19e3186b17e0826995aa38a4fa493f51f8
Husan <samadovxusan9013@gmail.com>
2025-04-10 21:25:50 +0500

    Add bookingData

--- Barber.Domain/Entities/BookingData.cs
+++ Barber.Domain/Entities/BookingData.cs
@@ -2,5 +2,10 @@ namespace Barber.Domain.Entities;
 
 public class BookingData
 {
-    
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid BarberId { get; set; }
+    public string ServiceId { get; set; } = string.Empty;
+    public DateOnly Date { get; set; }
+    public TimeSpan AppointmentTime { get; set; }
 }
